87b36e43dc4d76d005662ab4493eb2591509570f
Nweke Joshua <jnweke54@gmail.com>
2022-09-14 12:57:46 -0700

    Completed header banner and About us section

---
+++ starter/main.js
@@ -0,0 +1,25 @@
+
+// /basic Es5 function decleration/ 
+// function name(){}
+
+var myname = "dj captain josh"
+
+function popsomething() {
+    alert( myname + " i have been coding since 12")
+    
+}
+
+// console.log(myname)
+
+console.log(document)
+var ourbody = document.querySelector(".ourparagraph")
+// ourbody.style.background = "black"
+
+var hamburger = document.querySelector(".hamburger-container" )
+// colsole.log(handburger)
+
+// handburger.classlist.add("yam")
+
+function addandremove(){
+    hamburger.classList.toggle('showburger')
+}
